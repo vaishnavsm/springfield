372f809919a557c179cdda349f8172e30abade37
Akshay Sharma <contact@imakshay.com>
2018-12-12 19:44:29 +0530

    add dummy end points. remove 5 sec wait

--- backend.py
+++ backend.py
@@ -7,7 +7,31 @@ app = Flask(__name__)
 def echo():
     return json.dumps({"started":"true"})
 
+@app.route("/add_rule")
+def add_rule(field, data, context):
+    """Adds rule.
+
+    Args:
+        field: Name of the field. eg. "Insured"
+        data: Data corresponding to the field. eg. "ABC, Inc"
+        context: Context around the data (including the data). eg. "Insured:\n\nABC, Inc and/or Associated"
+    """
+
+    pass
+
+@app.route("/extract")
+def extract(text, field):
+    """Extracts data.
+
+    Args:
+        text: Text extracted from document through OCR.
+        field: Field corresponding to which we need to extract data.
+
+    Returns:
+        data: Data corresponding to the field.
+    """
+
+    return 'XYZ'
+
 if __name__ == "__main__":
-    print('oh hello')
-    time.sleep(5)
     app.run(host='127.0.0.1', port=5122)
